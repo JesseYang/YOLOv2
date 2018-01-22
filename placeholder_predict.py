21f5b3cf13f19dea9a17406d639de90cc8aeef29
JesseYang <jesse.yang1985@gmail.com>
2018-01-23 07:25:36 +0800

    u

--- placeholder_predict.py
+++ placeholder_predict.py
@@ -5,14 +5,14 @@ import argparse
 import tensorflow as tf
 from tensorpack import *
 
-from train_lite import Model
+from darknet_yolo_lite import DarknetYoloLite
 
 def predict(args):
 
 
     with tf.Session() as sess:
 
-        model = Model(data_format='NHWC')
+        model = DarknetYoloLite(data_format='NHWC')
         image = tf.placeholder(dtype=tf.float32,
                                shape=[1, 416, 416, 3],
                                name='input_image')
@@ -45,19 +45,20 @@ def predict(args):
             model.build_graph(image, tx, ty, tw, th, tprob, spec_mask, truth_box, ori_shape)
 
         saver = tf.train.Saver()
-        saver.restore(sess, args.model)
+        saver.restore(sess, args.model_path)
 
         output = tf.get_default_graph().get_tensor_by_name("lite_output:0")
         output_ary = sess.run(output, feed_dict={image: np.zeros((1, 416, 416, 3))})
 
         print(output_ary.shape)
 
-        tf.train.write_graph(sess.graph_def, "./", "yolo.pb", as_text=False)
+        tf.train.write_graph(sess.graph_def, "./", args.output_path, as_text=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', help='path to the model file', required=True)
+    parser.add_argument('--model_path', help='path to the model file', required=True)
+    parser.add_argument('--output_path', help='path to the output file', default="output.pb")
 
     os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 
